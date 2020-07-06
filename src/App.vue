0d892c1c3fdf867d8834d3193c3c613c6f33766f
longsky <1986028533@qq.com>
2020-07-06 18:46:36 +0800

    把hot_fix的内容合并到主分支

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <div>这是在pre_release_20200706分支下的修改</div>
     <div>在hot-fix分支下对bug进行修复---</div>
     <div>在hot-fix分支下对bug进行修复，并使用sourcetree推送</div>
+    <div>合并hot_fix_yyyymmdd分支并使用sourcetree提交、推送</div>
   </div>
 </template>
 
